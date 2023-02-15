89c7c3aa7d2dad4fba0927f732dcccb833946965
Fox697 <ryan.meyer@bluewin.ch>
2023-02-15 12:53:07 +0100

    Update comments from humidity

--- humidity.m
+++ humidity.m
@@ -1,6 +1,7 @@
 function [humidity] = humidity()
 % misst die Feuchtigkeit in der Erde
-%   
+% Version: 0.1
+% Test-cases: 
 voltage = readVoltage(arduinoObj, "A0");
 humidity=voltage/3.2*100;
 end
