998eb531695a371e8b02f71dfbf3698c3fff92ed
Fox697 <ryan.meyer@bluewin.ch>
2023-02-15 16:14:35 +0100

    lalalalalla

--- LCD_update.m
+++ LCD_update.m
@@ -1,14 +1,70 @@
-function [] = LCD_update()
+function [] = LCD_update(reservoir_height,water,moisture,light_time,arduinoObj)
+%reservoir_height,water,moisture,light_time,
+
 % Function zum Update des LCD_Screen 
 % Version 0.1
 % Test-Cases:
 
-%Scan I2CBus
-%devBus=arduinoObj.scanI2CBus;
+%Testparamet
+% reservoir_height=0.6;
+% water=0.3;
+% moisture=40;
+% light_time=7200;
+% arduinoObj=arduino();
 
-%Objekte für Screen definieren
-LCDobj=device(arduinoObj,'I2CAddress',0X3E);
-RGBobj=device(arduinoObj,'I2CAddress',0X62);
+%Füllstand in % berechnen und als String
+str_filling=num2str(round(water/reservoir_height*100,0))+"%";
+
+%Lichtzeit in h und als String
+str_light=num2str(round(light_time/3600,0)) +"h";
+
+%Feuchtigkeit als string
+str_moisture=num2str(moisture)+"%";
+
+%Strings für zweite Zeile basteln
+%Feuchtigkeit als string und immer gleich lang (4 chars)
+% str_moisture=num2str(moisture);
+% if strlength(str_moisture) == 1
+%     str_moisture = str_moisture + " % ";
+% elseif strlength(str_moisture) == 2
+%     str_moisture = str_moisture + " %";
+% end
+% 
+% %Immer gleich langer String generieren (4 chars)
+% if strlength(str_filling) == 1
+%     str_filling = str_filling + " % ";
+% elseif strlength(str_filling) == 2
+%     str_filling = str_filling + " %";
+% end
+% %Immer gleich langer String generieren (4 chars)
+% if strlength(str_light) == 1
+%     str_light = str_light + " h ";
+% elseif strlength(str_light) == 2
+%     str_light = str_light + " h";
+% end
+% 
+% str_values=str_filling + "  " + str_moisture + "   " + str_light
+
+
+%Objekte LCD-Screen definieren
+LCD_Screen=groveLCD(arduinoObj);
+switchON(LCD_Screen);
+clearLCD(LCD_Screen);
+
+%Schreiben auf erste Linie (Titel)
+homeLCD(LCD_Screen);
+str_titles='H20   Soil   Sun';
+writeLCD(LCD_Screen,convertStringsToChars(str_titles))
+
+%Schreiben der Werte auf die zweite Linie
+setCursorLCD(LCD_Screen,2,1)
+writeLCD(LCD_Screen,convertStringsToChars(str_filling))
+setCursorLCD(LCD_Screen,2,7)
+writeLCD(LCD_Screen,convertStringsToChars(str_moisture))
+setCursorLCD(LCD_Screen,2,14)
+writeLCD(LCD_Screen,convertStringsToChars(str_light))
+
+clear LCD_Screen
 
 end
 
--- projekt_infm_1.m
+++ projekt_infm_1.m
@@ -12,11 +12,11 @@
 
 
 %Benutzerparameter
-humidity_limit = 30;      %Grenzwert für die Feuchtigkeit, Wert 0-100
-reservoir_height =               % Grenzwert für Wasserpegel
-light_limit = 8;              % Grenzwert für Lichtintensität
-time_limit_h = 12;          % Grenzwert für Sonnenstunden in Stunden
-water_time = 5;               %Zeitdauer eines "Giess-Intervalls" in Sekunden
+humidity_limit = 30;      % Grenzwert für die Feuchtigkeit, Wert 0-100
+reservoir_height = 0.5;   % Höhe von Behälter (in Meter)
+light_limit = 8;          % Grenzwert für Lichtintensität
+time_limit_h = 12;        % Grenzwert für Sonnenstunden in Stunden
+water_time = 5;           % Zeitdauer eines "Giess-Intervalls" in Sekunden
 
 
 
@@ -25,44 +25,45 @@ water_time = 5;               %Zeitdauer eines "Giess-Intervalls" in Sekunden
 % Output: I2C = LCD, D5 = Ventil, D2 Ausgang für Reset
 
 % Variablen
-time=0;         % timestamp zum Speichern der Laufzeit
+run_time=0;         % timestamp zum Speichern der Laufzeit
+light_time=0;        % Zeit wie lange Licht vorhanden war 
 moisture=0;       % enthält die gemessene Fechtigkeit
 light=0;           % Logisches 1 für Lampe eingeschaltet
 brightness=0;       % enthält die gemessene Lichtintensität
 water=0;          % enhält Wasserstand
 
 % Anschlüsse definieren
-arduinoObj = arduino("COM7", "Leonardo")
+arduinoObj = arduino("COM7", "Leonardo");
 configurePin(arduinoObj, "A0", "AnalogInput");
 configurePin(arduinoObj, "A1", "AnalogInput");
 configurePin(arduinoObj, "D6", "PWM");
-onfigurePin(arduinoObj, "D2", "DigitalOutput");
+configurePin(arduinoObj, "D2", "DigitalOutput");
 
 % Umrechnung der Benutzerparameter
-time_limit = time_limit_h*60*60*1000;       % Umrechnung von Stunden in millisekunden 
-water_limit_1 = reservoir_height*0.5;       % erster Grenzwert für 50% Füllstand
-water_limit_2 = reservoir_height*0.05;       % zweiter Grenzwert für 5% Füllstand
+time_limit = time_limit_h*60*60;       % Umrechnung von Stunden in Sekunden 
+water_limit_1 = reservoir_height*0.5;  % erster Grenzwert für 50% Füllstand
+water_limit_2 = reservoir_height*0.05; % zweiter Grenzwert für 5% Füllstand
 
 
 while (1)                        % Main-loop
-    while(time<=(24*60*60))      % wird nach 24h zurückgesetzt
-        water=waterlevel();       % Auftruf der Wasserstand-Mess-Function
+    while(run_time<=(24*60*60))      % wird nach 24h zurückgesetzt
+        water=waterlevel(arduinoObj);       % Auftruf der Wasserstand-Mess-Function
         if water < water_limit_2            % Wenn Wasserstand <5% LCD updaten und nicht giessen
-            %screen update
+            LCD_update(reservoir_height,water,moisture,light_time,arduinoObj);
 
         elseif water>=water_limit_1         % Wenn Wasserstand >=50% Feuchtigkeit messen
-            moisture=humidity();
+            moisture=humidity(arduinoObj);
             watering(humidity_limit,water_time);
             
         else                        % Ansonsten LCD updaten und Feuchtigkeit messen
-            %screen update
-            watering(humidity_limit,water_time);
+            LCD_update(reservoir_height,water,moisture,light_time,arduinoObj);
+            watering(humidity_limit,water_time,arduinoObj);
         end
 
         if light==0                       %überprüft Lichtintensität
             brightness=light_intensity;
             if brightness<light_limit || light==1       % betritt die Bedingung wenn die Lichtintensität zu gering ist
-                if time<time_limit
+                if run_time<time_limit
                     light=1;                % Licht ein und Merker setzen
                     writeDigitalPin(arduinoObj, "A0",1);
                 else
@@ -73,8 +74,8 @@ while (1)                        % Main-loop
         end
         
         pause(3600);
-        time=temporalCount(sec);
-        %screen update
+        run_time=temporalCount(sec);
+        LCD_update(reservoir_height,water,moisture,light_time,arduinoObj);
        
     end
     writeDigitalPin(arduinoObj, "D2",1);        % D2 auf "reset" Pin verbinden, reseted den Arduino nach einem Tag
