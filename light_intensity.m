a3d5770ce88187bcd152e3a248f16e405fbe24ba
Halbflauschmammut <beat_peter@hotmail.ch>
2023-02-15 16:27:48 +0100

    update, not finished

--- light_intensity.m
+++ light_intensity.m
@@ -1,7 +1,24 @@
 function [brightness] = light_intensity()
-%misst die Lichtintensität
+% misst die Lichtintensität
+% Version 0.1
+% Test-Parameter:
 
-voltage = readVoltage(arduinoObj, "A1");
+while valid==0
+    for i=1, i<=6, i++
+voltage[i,1] = readVoltage(arduinoObj, "A1");
+    end
+
+    i=1;
+    referenz=voltage(6,1)
+
+    for i=1, i<6, i++
+        diff=abs(referenz-voltage(i,1));
+        if diff>0.3
+            valid=0;
+        end
+    end
+end
+
 brightness=voltage/3.75*100;
 end
 
