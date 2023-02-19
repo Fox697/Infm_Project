49bdf971060eafb3b020f8d0a3d305eb4306542a
Halbflauschmammut <beat_peter@hotmail.ch>
2023-02-19 12:01:14 +0100

    function fertig gestellt

--- light_intensity.m
+++ light_intensity.m
@@ -4,21 +4,22 @@ function [brightness] = light_intensity()
 % Test-Parameter:
 
 while valid==0
+    valid=1;            % Bedingung zum verlassen des Loops setzen
     for i=1, i<=6, i++
-voltage[i,1] = readVoltage(arduinoObj, "A1");
+        voltage[i,1] = readVoltage(arduinoObj, "A1");       %Sensor auslesen und in Matrix speichern
+        pause(10);      % Delay
     end
 
     i=1;
-    referenz=voltage(6,1)
-
+    referenz=voltage(6,1)           % Referenzspannung bestimmen
     for i=1, i<6, i++
-        diff=abs(referenz-voltage(i,1));
-        if diff>0.3
+        diff=abs(referenz-voltage(i,1));        % Alle gemessenen Spannungen mit der Referenz vergleichen
+        if diff>0.3             % wenn eine Spannung zu stark abweicht wird die Messung wiederholt
             valid=0;
         end
     end
 end
 
-brightness=voltage/3.75*100;
+brightness=referenz/3.75*100;       % Umrechnen in Prozent
 end
 
